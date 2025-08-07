c57d32971e126a75b7a141f10b5f1a2450afaae0
Debasmita <debasmitapaul8398@gmail.com>
2025-08-07 14:27:28 +0600

    Update DeveloperController.php

--- app/Http/Controllers/DeveloperController.php
+++ app/Http/Controllers/DeveloperController.php
@@ -1,1 +1,43 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class DeveloperController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = User::where('role', 'Developer');
+
+        if ($request->filled('skills')) {
+            $query->where('skills', 'like', '%' . $request->skills . '%');
+        }
+
+        if ($request->filled('experience')) {
+            $query->where('experience', 'like', '%' . $request->experience . '%');
+        }
+
+        if ($request->filled('location')) {
+            $query->where('location', 'like', '%' . $request->location . '%');
+        }
+
+        if ($request->filled('availability')) {
+            $query->where('availability', $request->availability);
+        }
+
+        $developers = $query->get();
+
+        return view('developer.index', compact('developers'));
+    }
+}
+
+
+
+
+
+
+
 
